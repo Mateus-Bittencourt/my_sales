cafa39abf21e3bfdc4bff95f825052b8c7e7f237
Mateus <mateusmbittencourt@gmail.com>
2025-07-24 21:52:23 -0300

    feat: product controller

--- src/shared/middlewares/ErrorHandleMiddleware.ts
+++ src/shared/middlewares/ErrorHandleMiddleware.ts
@@ -4,16 +4,17 @@ import { NextFunction, Request, Response } from 'express'
 export default class ErrorHandleMiddleware {
   public static handleError(
     error: Error,
-    _req: Request,
-    res: Response,
+    _request: Request,
+    response: Response,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     _next: NextFunction
   ) {
     if (error instanceof AppError)
-      return res
+      return response
         .status(error.statusCode)
         .json({ type: 'error', message: error.message })
 
-    return res
+    return response
       .status(500)
       .json({ type: 'error', message: 'Internal server error' })
   }
