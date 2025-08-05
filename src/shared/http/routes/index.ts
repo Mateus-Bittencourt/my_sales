83cd8a7634267cdbc73c1b3d193a79aedf188524
Mateus <mateusmbittencourt@gmail.com>
2025-08-05 20:40:37 -0300

    feat: add static route

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -2,7 +2,8 @@ import productsRouter from '@modules/products/routes/ProductRoutes'
 import avatarsRouter from '@modules/users/routes/AvatarRoutes'
 import sessionsRouter from '@modules/users/routes/SessionRoutes'
 import usersRouter from '@modules/users/routes/UserRoutes'
-import { Router } from 'express'
+import express, { Router } from 'express'
+import uploadConfig from '@config/upload'
 
 const routes = Router()
 
@@ -14,5 +15,6 @@ routes.use('/sessions', sessionsRouter)
 routes.use('/products', productsRouter)
 routes.use('/users', usersRouter)
 routes.use('/avatars', avatarsRouter)
+routes.use('/uploads', express.static(uploadConfig.directory))
 
 export default routes
