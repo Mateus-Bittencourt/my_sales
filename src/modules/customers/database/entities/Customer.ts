2a67ea8f904fb60e0cba7dd015f31d434c9929cf
Mateus <mateusmbittencourt@gmail.com>
2025-08-27 19:28:55 -0300

    feat: customer migration, entity and repository

---
+++ src/modules/customers/database/entities/Customer.ts
@@ -0,0 +1,25 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm'
+
+@Entity('customers')
+export class Customer {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column({ type: 'text' })
+  name: string
+
+  @Column({ type: 'text' })
+  email: string
+
+  @CreateDateColumn({ type: 'timestamp' })
+  created_at: Date
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updated_at: Date
+}
